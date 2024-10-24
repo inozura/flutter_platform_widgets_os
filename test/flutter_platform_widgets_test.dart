61542637359a91769fbc3ea373c3bf1601f59c98
Novandra Zulfi Ramadhan <nodhzen@gmail.com>
2024-10-24 13:02:27 +0700

    feat: update main package name

--- test/flutter_platform_widgets_test.dart
+++ test/flutter_platform_widgets_test.dart
@@ -1,6 +1,6 @@
 // import 'package:test/test.dart';
 
-// import 'package:flutter_extended_platform_widgets/flutter_extended_platform_widgets.dart';
+// import 'package:flutter_platform_widgets_os/flutter_platform_widgets_os.dart';
 
 // void main() {
 //   test('adds one to input values', () {
